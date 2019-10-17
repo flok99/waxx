ef9b4f7ee5d7e9251f2f8be7dddb0143aab80f1a
Folkert van Heusden <mail@vanheusden.com>
2019-10-17 13:57:55 +0200

    client

---
+++ client.py
@@ -0,0 +1,109 @@
+#! /usr/bin/python3
+
+import fcntl
+import getopt
+import os
+import select
+import socket
+import subprocess
+import sys
+import traceback
+
+def usage():
+    print('-e x  program to invoke (Ataxx "engine")')
+    print('-i x  server to connect to')
+    print('-p x  port to connect to (usually 28028)')
+    print('-U x  username to use')
+    print('-P x  password to use')
+
+engine = None
+host = 'server.ataxx.org'
+port = 28028
+user = None
+password = None
+
+try:
+    optlist, args = getopt.getopt(sys.argv[1:], 'e:i:p:U:P:')
+
+    for o, a in optlist:
+        if o == '-e':
+            engine = a
+        elif o == '-i':
+            host = a
+        elif o == '-p':
+            port = int(a)
+        elif o == '-U':
+            user = a
+        elif o == '-P':
+            password = a
+        else:
+            print(o, a)
+
+except getopt.GetoptError as err:
+    print(err)
+    usage()
+    sys.exit(1)
+
+if user == None or password == None:
+    print('No user or password given')
+    usage()
+    sys.exit(1)
+
+while True:
+    try:
+        p = subprocess.Popen(engine, stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+        fl = fcntl.fcntl(p.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(p.stdout, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+
+        s.send(bytes('user %s' % user, encoding='utf8'))
+        s.send(bytes('password %s' % password, encoding='utf8'))
+
+        poller = select.poll()
+        poller.register(s.fileno(), select.POLLIN)
+        poller.register(p.stdout.fileno(), select.POLLIN)
+
+        terminate = False
+
+        while not terminate:
+            events = poller.poll(-1)
+            if events == None:
+                print('Poller returned error?')
+                break
+
+            for fd, flag in events:
+                if fd == s.fileno():
+                    dat = s.recv(4096)
+                    if dat == None:
+                        terminate = True
+                    else:
+                        print('server: %s' % dat.decode())
+                        p.stdin.write(dat.decode())
+                        p.stdin.flush()
+
+                elif fd == p.stdout.fileno():
+                    dat = p.stdout.read()
+                    if dat == None:
+                        terminate = True
+                    else:
+                        print('engine: ', dat)
+                        s.send(dat.encode('utf-8'))
+
+                else:
+                    print('Unexpected error ', fd, flag)
+                    break
+
+        s.close()
+        del s
+
+        p.stdout.close()
+        p.terminate()
+        p.wait()
+        del p
+
+    except Exception as e:
+        print('failure: %s' % e)
+        traceback.print_exc(file=sys.stdout)
+        break
